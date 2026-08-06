0b51ce9438c17da4d21b1d296c7c81f46aea2581
patrickalecoliveira10-cell <patrick.alec.oliveira10@gmail.com>
2026-08-06 12:45:56 -0300

    Update marketScanner.js

--- scanner/src/scanner/marketScanner.js
+++ scanner/src/scanner/marketScanner.js
@@ -34,6 +34,9 @@ class MarketScanner {
     }
 
     logger.info('Starting Market Scanner...');
+    // Log config state at startup to confirm indicator flags
+    const cfg = config.indicators;
+    logger.info(`[SCANNER-CONFIG] macd=${cfg.macd} adx=${cfg.adx} ema=${cfg.ema} bb=${cfg.bollinger} st=${cfg.supertrend} psar=${cfg.psar} stoch=${cfg.stochastic} vwap=${cfg.vwap} obv=${cfg.obv}`);
     this.isRunning = true;
     this.stats.startTime = Date.now();
 
